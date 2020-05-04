d6c2bd0582faf5e5ec2ca4bd72f150b1ffa50eef
Boulaich Karim <karim.boulaich.1@ulaval.ca>
2020-05-03 20:30:04 -0400

    add

--- front/src/Socket.tsx
+++ front/src/Socket.tsx
@@ -24,6 +24,7 @@ class  Socket {
         if (this.socket && this.socket.connected) {
             return ;
         }
+        console.log(process.env);
         const port = (process.env.NODE_ENV === "production") ? '80' : '8080';
         this.socket = io('http://localhost:' + port);
         return new Promise(resolve => {
